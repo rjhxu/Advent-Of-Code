234242f2d094b05fba3425a72e697fd6930d9ff2
RyanjXu <ryanjiahuaxu@gmail.com>
2024-12-07 17:52:27 -0500

    Update star2.py

--- 2024/day1/star2.py
+++ 2024/day1/star2.py
@@ -1,7 +1,7 @@
 id1 = []
 id2 = []
 sum = 0
-for line in open("day1/input.txt", "r"):
+for line in open("2024/day1/input.txt", "r"):
     ltokens = line.split()
     id1.append(int(ltokens[0]))
     id2.append(int(ltokens[1]))
