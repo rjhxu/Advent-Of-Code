1a12b44ef3d74e0cc6ef1b08f6837bde8264a6ef
RyanjXu <ryanjiahuaxu@gmail.com>
2024-12-08 12:48:22 -0500

    Create day2

---
+++ 2023/day2/day2star1.java
@@ -0,0 +1,35 @@
+package day2;
+
+import java.util.*;
+import java.io.*;
+
+public class day2star1 {
+    public static int sum  = 0;
+    public static HashMap<String, Integer> mp = new HashMap<>();
+    public static void main(String[] ILOVEPENISPENISPENIS) throws IOException {
+        Scanner in = new Scanner(new FileReader("2023/day2/input.txt"));
+        mp.put("blue", 14);
+        mp.put("green", 13);
+        mp.put("red", 12);
+        for(int i =1; i<101; i++) {
+            String[] temp = in.nextLine().split(":");
+            String line = temp[1].trim();
+            check(line, i);
+        }
+        System.out.println(sum);
+        in.close();
+    }
+    public static void check(String str, int id) {
+        String[] arr1 = str.split(";");
+        for(int i =0; i<arr1.length; i++) {
+            String[] arr2 = arr1[i].split(",");
+            for(int j = 0; j<arr2.length; j++) {
+                String[] arr3 = arr2[j].trim().split(" ");
+                if(Integer.parseInt(arr3[0]) > mp.get(arr3[1])) {
+                    return;
+                }
+            }
+        }
+        sum+=id;
+    }
+}
---
+++ 2023/day2/day2star2.java
@@ -0,0 +1,40 @@
+package day2;
+
+import java.util.*;
+import java.io.*;
+
+public class day2star2 {
+    public static long sum  = 0;
+    public static void main(String[] ILOVEPENISPENISPENIS) throws IOException {
+        Scanner in = new Scanner(new FileReader("2023/day2/input.txt"));
+        for(int i =1; i<101; i++) {
+            String[] temp = in.nextLine().split(":");
+            String line = temp[1].trim();
+            check(line);
+        }
+        System.out.println(sum);
+        in.close();
+    }
+    public static void check(String str) {
+        int bm = 0, gm = 0, rm = 0;
+        String[] arr1 = str.split(";");
+        for(int i =0; i<arr1.length; i++) {
+            String[] arr2 = arr1[i].split(",");
+            for(int j = 0; j<arr2.length; j++) {
+                String[] arr3 = arr2[j].trim().split(" ");
+                switch(arr3[1].charAt(0)) {
+                    case 'b':
+                        if(Integer.parseInt(arr3[0])>bm) {bm=Integer.parseInt(arr3[0]);}
+                        break;
+                    case 'g':
+                        if(Integer.parseInt(arr3[0])>gm) {gm=Integer.parseInt(arr3[0]);}
+                        break;
+                    case 'r':
+                        if(Integer.parseInt(arr3[0])>rm) {rm=Integer.parseInt(arr3[0]);}
+                        break;
+                }
+            }
+        }
+        sum+=(bm*gm*rm);
+    }
+}
