d838459660ab3095557feee36e859d783fcf5d3c
Ryan <ryanjiahuaxu@gmail.com>
2025-12-03 01:38:57 -0500

    readability

--- 2025/day3/star2.py
+++ 2025/day3/star2.py
@@ -2,18 +2,14 @@ total_joltage_output = 0
 with open("2025/day3/input.txt") as file:
     for line in file:
         v = list(map(int, list(line.strip())))
-        joltage = 0
         largest_num = max(v[:-11])
-        joltage += (10**11)*largest_num
+        joltage = (10**11)*largest_num
 
         for i in range(10, -1, -1):
             idx = v.index(largest_num)
             v = v[idx+1:]
-            idx = 0
             largest_num = max(v[:-i if i>0 else None])
             joltage+=(10**i)*largest_num
         total_joltage_output+=joltage
-
-
 
 print(total_joltage_output)
